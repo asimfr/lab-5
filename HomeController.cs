088a46fc07fb2d1e76aed88793581e8261a5719c
asimfr <63205846+asimfr@users.noreply.github.com>
2020-04-13 13:00:39 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,26 @@
+﻿using JSon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JSon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static Student[] empList = new Student[]
+{
+new Student() {Name="Muhammad Ahmad", Regno="bse173099" , Cgpa=3.9 },
+new Student() {Name="Usman khan", Regno="bse173097" , Cgpa=3.0 },
+new Student() {Name="Asfand ali", Regno="bse173092" , Cgpa=2.3 },
+new Student() {Name="Hamza farooq",Regno="bse173093" , Cgpa=3.5 },
+};
+
+
+        public JsonResult GetEmpData()
+        {
+            return Json(empList);
+        }
+    }
+}
